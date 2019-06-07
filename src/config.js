cee1df4216b13bba94306db05003c5a071d7c8f3
gmora08 <gmora008@gmail.com>
2019-06-07 16:06:33 -0500

    set config for logs

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
   port: process.env.PORT,
+  log_level: process.env.LOG_LEVEL,
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
+import pino from 'pino';
+import expressPino from 'express-pino-logger';
 
-import { port } from './config';
+import { port, log_level } from './config';
+
+const logger = pino({level: log_level || 'info'});
+const expressLogger = expressPino({ logger });
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(expressLogger);
 
 app.get('/', (req, res) => {
+  logger.debug('Calling res.send')
   res.send('Hello World')
 })
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  logger.info('Server running on port %d', port)
 });
