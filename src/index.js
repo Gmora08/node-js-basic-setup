15a9f539939c2af310a49e8ff48fa7764883c635
gmora08 <gmora008@gmail.com>
2019-06-07 14:56:54 -0500

    setup express with helmet, bodyParser and cors libraries

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+
 import { port } from './config';
-console.log(`Hello Node.js project. ${port}`);
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
